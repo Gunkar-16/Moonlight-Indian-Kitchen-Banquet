db1e1cc4e2bfda5a6100fc989a65dd2996c0854f
Gunkar-16 <16.gunkar@gmail.com>
2026-04-23 23:05:32 -0600

    updated the display page more and added a bit responsiveness

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./*.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2438f0',
+      },
+    },
+  },
+  plugins: [],
+}
